d20ba31a3ba1a44a924708affe40b4371b5516ae
Osama Abdul Sabboor <56756079+osamaabdulsabboor@users.noreply.github.com>
2020-11-18 22:44:49 +0500

    commit

--- server/index.js
+++ server/index.js
@@ -7,23 +7,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
-
 mongoose.connect(
-    process.env.MONGODB_CONNECTION_STRING,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    },
-    (err) => {
-      if (err) throw err;
-      console.log("Mongodb connection established");
-    }
-  );
-  
-  app.use("/users", require("./routes/userRouter"));
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Mongodb connection established");
+  }
+);
+
+app.use("/users", require("./routes/userRouter"));
