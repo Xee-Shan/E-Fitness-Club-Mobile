5b2c75f1eb99e8a7341d6d86a78317ab885cc167
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2020-12-21 23:06:41 +0500

    ll;'

--- client/screens/user/E-Commerece/CartScreen.js
+++ client/screens/user/E-Commerece/CartScreen.js
@@ -95,7 +95,7 @@ export default function CartScreen({ navigation }) {
   async function handleOrder() {
     const itemId = await AsyncStorage.getItem("item-id");
     if (cart?.length > 0) {
-      const item = cart.find((arr) => arr.id === itemId);
+      const item = cart.find((arr) => arr.id === JSON.parse(itemId));
       if (item.quantity <= product.quantity - orderedQuantity) {
         const token = await AsyncStorage.getItem("auth-token");
         axios.post("http://10.0.2.2:5002/orders/placeOrder", cart, {
