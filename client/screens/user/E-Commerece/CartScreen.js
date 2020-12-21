d9e102ab78dc5c6a479ac6487ccbe75aafb97c8d
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2020-12-21 23:14:12 +0500

    Update CartScreen.js

--- client/screens/user/E-Commerece/CartScreen.js
+++ client/screens/user/E-Commerece/CartScreen.js
@@ -94,9 +94,14 @@ export default function CartScreen({ navigation }) {
 
   async function handleOrder() {
     const itemId = await AsyncStorage.getItem("item-id");
-    console.log(typeof itemId);
     if (cart?.length > 0) {
       const item = cart.find((arr) => arr.id === itemId);
+      console.log(item);
+      console.log(typeof item.quantity);
+      console.log(typeof product.quantity);
+      console.log(typeof orderedQuantity);
+
+
       if (item.quantity <= product.quantity - orderedQuantity) {
         const token = await AsyncStorage.getItem("auth-token");
         axios.post("http://10.0.2.2:5002/orders/placeOrder", cart, {
