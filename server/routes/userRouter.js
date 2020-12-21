5a49477798065ac5a98bf7998197820f677aebc9
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2020-12-21 21:48:25 +0500

    Update userRouter.js

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -321,9 +321,7 @@ router.post("/addToCart/:myQuantity", auth, async (req, res) => {
 
         user.cart.map((cart) => {
           if (cart.id === req.body._id) {
-            Number(cart.quantity);
-            console.log(typeof Number(cart.quantity));
-            cart.quantity += myQuantity;
+            Number(cart.quantity)=Number(cart.quantity)+ myQuantity;
             console.log(cart.quantity);
           }
         });
