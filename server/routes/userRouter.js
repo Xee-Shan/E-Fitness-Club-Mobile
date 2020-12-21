b1fe64578bc72ff31f986bf4664cba9216d898d0
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2020-12-21 22:06:09 +0500

    sdf

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -321,7 +321,7 @@ router.post("/addToCart/:myQuantity", auth, async (req, res) => {
 
         user.cart.map((cart) => {
           if (cart.id === req.body._id) {
-            cart.quantity=myQuantity + + cart.quantity;
+            cart.quantity=+myQuantity+cart.quantity;
             console.log(cart.quantity);
           }
         });
