8cc623ae7cd0e9f39ada282d41356a68a3ae17f9
Osama Abdul Sabboor <56756079+osamaabdulsabboor@users.noreply.github.com>
2021-01-17 18:59:12 +0500

    update

--- client/screens/SplashScreen.js
+++ client/screens/SplashScreen.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function SplashScreen({navigation}) {
-    return (
-        <View>
-            <Text>E-Fitness Club</Text>
-            <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate("Home")}>
-            <Text>Home</Text>
-            </TouchableOpacity>
-        </View>
-    )
+export default function SplashScreen({ navigation }) {
+  return (
+    <View>
+      <Text>E-Fitness Club</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Home")}
+      >
+        <Text>Click Me</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button:{
-        flexDirection:"row",
-        backgroundColor:"blue",
-        width:"10%"
-    }
-})
+  button: {
+    flexDirection: "row",
+    backgroundColor: "black",
+    width: "15%",
+  },
+});
